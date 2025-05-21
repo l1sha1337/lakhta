319a36bf6bc69ac01867da501d8d08efcf9dc79d
Vladislav Malakhin <vmalakhin@xiag.ch>
2025-05-21 10:49:49 +0700

    allow filtering by time and tickets count

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import json
-import sys
 
 import requests
 import telebot
@@ -7,8 +6,15 @@ from telebot import types
 import time
 import datetime as dt
 
-need_date = None
+required_date = '24.05.2025'
+required_quantity = 2
+required_time = '10:30'
+bot_started = False
+
+
 def load_available_times():
+    global required_time, required_quantity, required_date
+
     url = 'https://tickets.lakhta.events/api/no-scheme'
     request_body = {'hash': '23FA307410B1F9BE84842D1ABE30D6AB48EA2CF8'}
     req = requests.post(url, json = request_body)
@@ -17,44 +23,71 @@ def load_available_times():
     calendar = (data["response"]["calendar"])
     results = []
     for item in calendar:
-        if (need_date is not None) and (need_date != item['day']):
+        if (required_date is not None) and (required_date != item['day']):
             continue
         for schedule in item['_time']:
-            if int(schedule['quantity']) == 0:
-                free_time = {'date': item['day'], 'time': schedule['time'], 'quantity': schedule['quantity']}
-                results.append(free_time)
+            if (required_quantity is not None) and (int(schedule['quantity']) < int(required_quantity)):
+                continue
+            if (required_time is not None) and (required_time != schedule['time']):
+                continue
+            free_time = {'date': item['day'], 'time': schedule['time'], 'quantity': schedule['quantity']}
+            results.append(free_time)
     return results
 
+bot = telebot.TeleBot('7997374049:AAG4F4reje3Bn_xBivGAaChc9kkymJErEAU')
 
-bot = telebot.TeleBot('7427442280:AAGTi4xAjbIsuoB0Gag2-Fjcv8PcnOrEtDc')
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    global stop_bot
-    stop_bot = True
+    global bot_started
+    bot_started = True
+    info_message = 'Вы подписались на информацию о билетах на дату: ' + required_date
+    if required_time:
+        info_message += ', время: ' + required_time
+    if required_quantity:
+        info_message += ', количество билетов: ' + str(required_quantity)
+    bot.send_message(message.from_user.id, info_message)
     results = load_available_times()
     if len(results) > 0:
         for available_time in results:
-            if stop_bot == False:
+            if not bot_started:
                 break
             url = 'https://tickets.lakhta.events/event/23FA307410B1F9BE84842D1ABE30D6AB48EA2CF8'
             url += '/' + dt.datetime.strptime(available_time['date'], "%d.%m.%Y").strftime("%Y-%m-%d")
             url += '/' + available_time['time']
             print(url)
-            bot.send_message(message.from_user.id, 'Ссылка на покупку билетов на ' + available_time['date'] + ' ' + available_time['time'] + " " + url)
-            time.sleep(1)
+            bot.send_message(
+                message.from_user.id,
+                'Ссылка на покупку билетов на ' + available_time['date']
+                + ' ' + available_time['time']
+                + ' свободных билетов: ' + available_time['quantity']
+                + " " + url
+            )
+    time.sleep(60)
+
 
 @bot.message_handler(commands=['stop'])
 def stop_message(message):
-    global stop_bot
-    stop_bot = False
-    bot.send_message(message.chat.id, 'Бот остановлен!')
+    global bot_started
+    if bot_started:
+        bot_started = False
+        bot.send_message(message.chat.id, 'Вы отписалить от рассылки о билетах!')
+    else:
+        bot.send_message(message.chat.id, 'Вы не были подписаны на рассылку!')
+
 @bot.message_handler(commands=['info'])
+@bot.message_handler()
 def info(message):
-    markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton("Сайт Лахта центр", url='https://lakhta.center/')
-    markup.add(button1)
     button2 = types.InlineKeyboardButton("Мой ютуб", url='https://youtube.com/@l3sha1337?si=PoAdsN4k3DtORM--')
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(button1)
     markup.add(button2)
-    bot.send_message(message.chat.id,"Привет, тут ссылки на сайт с котрым я роботаю и на мой ютуб".format(message.from_user),reply_markup=markup)
+    bot.send_message(
+        message.chat.id,
+        'Привет, тут ссылки на сайт, с котрым я роботаю и на мой ютуб'.format(message.from_user),
+        reply_markup=markup
+    )
 
 bot.polling(none_stop=True, interval=0)
